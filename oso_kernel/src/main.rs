2faa6847ca6bb519e87730973e1176fb95ffa942
sugiura-hiromiti <sugiura130418@icloud.com>
2025-03-09 21:33:32 +0900

    kernel: fix panic handler behivor
    from `asm!("hlt")` to just loop because of distinguishing `asm!("hlt")` in kernel entry point

--- oso_kernel/src/main.rs
+++ oso_kernel/src/main.rs
@@ -2,9 +2,23 @@
 #![no_main]
 
 use core::arch::asm;
+use oso_util::graphic::FrameBufConf;
 
 #[unsafe(no_mangle)]
-pub extern "sysv64" fn kernel_main(frame_buf_base: u64, frame_buf_size: u64,) {
+/// TODO:
+/// `extern "sysv64"` を除く事はできるのか?
+/// カーネルを呼び出すのはうまく行っているようだが、bootloaderとkernel側で sysv64 abi
+/// に則った関数として扱わないと引数の受け渡しがうまくいかない
+/// elf形式でコンパイルするので、恐らくその時に (x86環境では)sysv64 abi
+/// が強制されているのではないか？
+pub extern "sysv64" fn kernel_main(frame_buf_conf: FrameBufConf,) {
+	let f_buf =
+		unsafe { core::slice::from_raw_parts_mut(frame_buf_conf.base, frame_buf_conf.size,) };
+
+	f_buf.iter_mut().enumerate().for_each(|(i, frame,)| {
+		*frame = ((i * 100) % 0x100) as u8;
+	},);
+
 	loop {
 		unsafe {
 			asm!("hlt");
@@ -13,10 +27,10 @@ pub extern "sysv64" fn kernel_main(frame_buf_base: u64, frame_buf_size: u64,) {
 }
 
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo,) -> ! {
+fn panic(_info: &core::panic::PanicInfo,) -> ! {
 	loop {
-		unsafe {
-			asm!("hlt");
-		}
+		// unsafe {
+		// 	asm!("hlt");
+		// }
 	}
 }
