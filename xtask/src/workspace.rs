5f23eff7e656e14e8136059f895ce488b0790a4c
sugiura-hiromiti <sugiura130418@icloud.com>
2025-04-15 16:02:19 +0900

    xtask: add test

--- xtask/src/workspace.rs
+++ xtask/src/workspace.rs
@@ -67,92 +67,6 @@ impl OsoWorkSpace {
 
 		Ok(Self { root, loader, kernel, },)
 	}
-
-	pub fn post_process(&self,) -> Rslt<(),> {
-		// // xtaskクレートのプロジェクトルートに戻る
-		// env::set_current_dir(&self.xtask_root,)?;
-		//
-		// // パスの用意
-		// let mount_point = self.mount_point()?;
-		// let img_path = self.img_path()?;
-		//
-		// // 前回実行時のものを削除
-		// if mount_point.exists() {
-		// 	Command::new("rm",).arg("-rf",).arg(&mount_point,).run()?;
-		// }
-		// if img_path.exists() {
-		// 	Command::new("rm",).arg("-rf",).arg(&img_path,).run()?;
-		// }
-		//
-		// // raw disk imageを作成
-		// let mut create_img = Command::new("qemu-img",);
-		// create_img.args(["create", "-f", "raw",],);
-		//
-		// match &self.arch {
-		// 	Architecture::X86_64 => create_img.arg(&img_path,).arg("200m",),
-		// 	Architecture::Aarch64 => todo!(),
-		// };
-		// create_img.run()?;
-		//
-		// // disk.imgをフォーマット
-		// Command::new("mkfs.fat",)
-		// 	.args(["-n", "'OSO'", "-s", "2", "-f", "2", "-R", "32", "-F", "32",],)
-		// 	.arg(&img_path,)
-		// 	.run()?;
-		//
-		// // マウントポイント作成 & マウント
-		// Command::new("mkdir",).arg("-p",).arg(&mount_point,).run()?;
-		// let mounted_disk = Command::new("hdiutil",)
-		// 	.args(["attach", "-imagekey", "diskimage-class=CRawDiskImage", "-nomount",],)
-		// 	.arg(&img_path,)
-		// 	.output()?
-		// 	.stdout;
-		// let mounted_disk = unsafe { String::from_utf8_unchecked(mounted_disk,) };
-		// let mounted_disk = mounted_disk.trim().to_string();
-		//
-		// Command::new("mount",)
-		// 	.args(["-t", "msdos",],)
-		// 	.arg(&mounted_disk,)
-		// 	.arg(&mount_point,)
-		// 	.run()?;
-		//
-		// // bootloader, kernelを配置
-		// Command::new("mkdir",).arg("-p",).arg(mount_point.join("efi/boot",),).run()?;
-		// match &self.arch {
-		// 	Architecture::X86_64 => {
-		// 		Command::new("cp",)
-		// 			.args([
-		// 				self.root.join(&self.loader.build_artifact,),
-		// 				mount_point.join("efi/boot/bootx64.efi",),
-		// 			],)
-		// 			.run()?;
-		// 		Command::new("cp",)
-		// 			.args([
-		// 				self.root.join(&self.kernel.build_artifact,),
-		// 				mount_point.join("oso_kernel.elf",),
-		// 			],)
-		// 			.run()?;
-		// 	},
-		// 	Architecture::Aarch64 => todo!(),
-		// }
-		//
-		// // unmount
-		// Command::new("hdiutil",).args(["detach", &mounted_disk,],).run()?;
-		// Ok((),)
-		todo!()
-	}
-
-	// fn mount_point(&self,) -> Rslt<PathBuf,> {
-	// 	let path = PathBuf::from_str(MOUNT_POINT,)?;
-	// 	let path = path.join("mnt",);
-	// 	Ok(path,)
-	// }
-	//
-	// fn img_path(&self,) -> Rslt<PathBuf,> {
-	// 	let path = PathBuf::from_str(MOUNT_POINT,)?;
-	// 	let path = path.join("disk.img",);
-	// 	Ok(path,)
-	// }
 }
 
 fn de_toml(path: &Path,) -> Rslt<Table,> {
@@ -160,34 +74,6 @@ fn de_toml(path: &Path,) -> Rslt<Table,> {
 	let table = toml_str.parse::<Table>()?;
 	Ok(table,)
 }
-
-// fn executable_location(crate_root: &Path, target: &String, crate_name: &String,) ->
-// Rslt<PathBuf,> { 	let out = if target.contains(".json",) {
-// 		let file = fs::File::open(&crate_root.join(target,),)?;
-// 		let reader = BufReader::new(file,);
-// 		let json: serde_json::Value = serde_json::from_reader(reader,)?;
-// 		let serde_json::Value::Array(opts,) = &json["post-link-args"]["ld.lld"] else {
-// 			panic!("your {target}[\"post-link-args\"][\"ld.lld\"] is not array that must be array");
-// 		};
-// 		let out = opts
-// 			.iter()
-// 			.find_map(|v| {
-// 				let opt = v.as_str().unwrap();
-// 				if &opt[..2] == "-o" { Some(&opt[2..],) } else { None }
-// 			},)
-// 			.expect(&format!(
-// 				"you need to specify name of build artifact explicitly in \
-// 				 {target}[\"post-link-args\"][\"ld.lld\"]",
-// 			),);
-// 		out.to_string()
-// 	} else if target.contains("uefi",) {
-// 		format!("target/{target}/debug/{crate_name}.efi")
-// 	} else {
-// 		format!("target/{target}/debug/{crate_name}")
-// 	};
-//
-// 	Ok(PathBuf::from_str(&out,)?,)
-// }
 
 fn oso_root(path: &Path,) -> PathBuf {
 	let p: PathBuf = path
@@ -229,4 +115,18 @@ pub fn detect_build_artifact(json: serde_json::Value,) -> Rslt<PathBuf,> {
 		),)?;
 
 	Ok(PathBuf::from_str(out,)?,)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn crate_new() -> Rslt<(),> {
+		let loader_root = std::env::current_dir()?.parent().unwrap().to_path_buf().join(LOADER,);
+		let n = Crate::new(&loader_root,)?;
+		assert_eq!(n.name, "oso_loader");
+		println!("{n:#?}");
+		Ok((),)
+	}
 }
