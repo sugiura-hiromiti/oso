c3179388839a501faead4b31d875c5abce9f101a
sugiura-hiromiti <sugiura130418@icloud.com>
2025-04-17 17:57:40 +0900

    impl guid

--- oso_loader/src/chibi_uefi/guid.rs
+++ oso_loader/src/chibi_uefi/guid.rs
@@ -1,4 +1,22 @@
+use crate::Rslt;
+use crate::error::OsoLoaderError;
+use crate::raw::types::Guid;
+use alloc::string::ToString;
+
 #[macro_export]
 macro_rules! guid {
 	($s:literal) => {};
+}
+
+impl Guid {
+	pub fn from_str(s: impl AsRef<str,>,) -> Rslt<Self,> {
+		let s = s.as_ref();
+		let len = s.len();
+
+		for i in  {
+
+		}
+
+		todo!()
+	}
 }
--- oso_loader/src/error.rs
+++ oso_loader/src/error.rs
@@ -2,9 +2,11 @@
 //! TODO: アロケータを実装したら`oso_util`に移管
 use alloc::format;
 use alloc::string::String;
+use alloc::string::ToString;
 use core::error::Error;
 use core::fmt::Debug;
 use core::fmt::Display;
+use core::num::ParseIntError;
 
 #[derive(Debug,)]
 pub enum OsoLoaderError {
@@ -33,5 +35,11 @@ impl From<goblin::error::Error,> for OsoLoaderError {
 impl From<OsoLoaderError,> for core::fmt::Error {
 	fn from(value: OsoLoaderError,) -> Self {
 		core::fmt::Error
+	}
+}
+
+impl From<ParseIntError,> for OsoLoaderError {
+	fn from(value: ParseIntError,) -> Self {
+		Self::Uefi(value.to_string(),)
 	}
 }
