428f09cc170d3466129afe1616d7a47dc2739118
sugiura-hiromiti <sugiura130418@icloud.com>
2025-06-04 07:48:02 +0900

    implementing config table manipulation

--- oso_loader/src/raw/table.rs
+++ oso_loader/src/raw/table.rs
@@ -1,5 +1,3 @@
-use alloc::vec::Vec;
-
 use super::service::BootServices;
 use super::service::RuntimeServices;
 use super::types::Char16;
@@ -44,18 +42,36 @@ pub struct ConfigTable {
 }
 
 pub struct ConfigTableStream {
-	current_index: usize,
 	max_index:     usize,
 	config_tables: Option<NonNull<ConfigTable,>,>,
 }
 
+impl ConfigTableStream {
+	fn config_table_with(&self, guid: Guid,) -> Rslt<Option<&mut ConfigTable,>,> {
+		if self.config_tables.is_none() {
+			return Ok(None,);
+		}
+
+		self.config_tables.iter().map(|config_table| {
+			let mut vendor_table = None;
+			for i in 0..self.max_index {
+				let target_ct = unsafe { config_table.as_ptr().add(i,) };
+				if unsafe { config_table.as_ref().vendor_guid } == guid {}
+			}
+			todo!()
+		},);
+
+		todo!()
+	}
+}
+
 impl ConfigTable {
-	pub fn get_config_table_with(&self, guid: Guid,) -> Rslt<Option<&mut ConfigTable,>,> {
+	pub fn config_table_with(&self, guid: Guid,) -> Rslt<Option<&mut ConfigTable,>,> {
 		todo!()
 	}
 
 	pub fn get_device_tree(&self,) -> Rslt<Option<&mut ConfigTable,>,> {
-		self.get_config_table_with(DEVICE_TREE_TABLE_GUID,)
+		self.config_table_with(DEVICE_TREE_TABLE_GUID,)
 	}
 }
 
@@ -63,6 +79,7 @@ pub const DEVICE_TREE_TABLE_GUID: Guid = guid!("b1b621d5-f19c-41a5-830b-d9152c69
 
 impl SystemTable {
 	pub fn get_config_tables(&self,) -> Rslt<ConfigTableStream,> {
-       todo!()
-   }
+		let config_tables = NonNull::new(self.config_tables,);
+		Ok(ConfigTableStream { max_index: self.config_table_count, config_tables, },)
+	}
 }
