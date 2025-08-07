36afbe6b709a58a68f93b84dc57995046f075ca3
sugiura-hiromiti <sugiura130418@icloud.com>
2025-08-08 08:43:44 +0900

    refactor(proc_macro): implement proper MacroDef parser
    - Replace step-based token parsing with structured syn::parse approach
    - Add ParamChunk Parse implementation for cleaner parameter handling
    - Remove commented-out legacy parsing code
    - Improve error messages for invalid macro kinds (fn_style, derive, attr)

--- oso_proc_macro_helper/src/lib.rs
+++ oso_proc_macro_helper/src/lib.rs
@@ -17,22 +17,51 @@ struct ParamChunk {
 	colon: syn::Token![,],
 }
 
+impl syn::parse::Parse for ParamChunk {
+	fn parse(input: syn::parse::ParseStream,) -> syn::Result<Self,> {
+		Ok(Self { param: input.parse()?, colon: input.parse()?, },)
+	}
+}
+
 impl syn::parse::Parse for MacroDef {
 	fn parse(input: syn::parse::ParseStream,) -> syn::Result<Self,> {
-		input.step(|c| match c.token_tree() {
-			Some((TokenTree::Ident(target,), next,),) => {
-				let XXXXXXXXX = match target.to_string().as_str() {
-					"fn_style" | "derive" | "attr" => {
-						ParamChunk { param: target, colon: c.token_stream, }
-					},
-					a => Err(c.error(format!(
-						r#"expected one of "fn_style", "derive", "attr". found {a} "#
-					),),),
-				};
-				todo!()
+		let macro_kind: ParamChunk = input.parse()?;
+
+		let macro_kind_raw = macro_kind.param.to_string();
+		let macro_kind_raw = macro_kind_raw.as_str();
+		match macro_kind_raw {
+			"fn_style" => Self::FnStyle { name: input.parse()?, param_item: input.parse()?, },
+			"derive" => Self::Derive {
+				name:       input.parse()?,
+				param_item: input.parse()?,
+				param_attr: input.parse_terminated()?,
 			},
-			_ => unimplemented!(),
-		},);
+			"attr" => Self::Attr {
+				name:       input.parse()?,
+				param_attr: input.parse()?,
+				param_item: input.parse()?,
+			},
+			_ => {
+				return Err(input.error(format!(
+					"expected one of fn_style, derive, attr. found {macro_kind_raw}"
+				),),);
+			},
+		};
+
+		// input.step(|c| match c.token_tree() {
+		// 	Some((TokenTree::Ident(target,), next,),) => {
+		// 		let XXXXXXXXX = match target.to_string().as_str() {
+		// 			"fn_style" | "derive" | "attr" => {
+		// 				ParamChunk { param: target, colon: c.t	oken_stream, }
+		// 			},
+		// 			a => Err(c.error(format!(
+		// 				r#"expected one of "fn_style", "derive", "attr". found {a} "#
+		// 			),),),
+		// 		};
+		// 		todo!()
+		// 	},
+		// 	_ => unimplemented!(),
+		// },);
 		todo!()
 	}
 }
